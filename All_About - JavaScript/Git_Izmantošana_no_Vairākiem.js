ed8fa176c388e816d281e4fbc399b05b07f49072
Zane <zanezeltina32@inbox.lv>
2023-04-05 08:56:34 +0300

    Working with Git

---
+++ All_About - JavaScript/Git_Izmantošana_no_Vairākiem.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Ja mums ir viens dators mājās, bet otrs ofisā, un mēs gribam no abiem strādāt pie viena projekta, pastrādāt ofisā, atnākt mājās, atjaunināt projektu un turpināt arto strādāt no mājas datora, tad mums tam ir: Git versiju kontrole un Git Hub - attālinātā failu glabātuve. */
+
+/* Ja esam pastrādājuši pie viena datora un tagad gribam strādāt pie tā paša projekta no cita datora, tad mums sākumā ir jāiekopē esošais projekts no Git Hub repozitorija jaunajā datorā, tajā no kura mēs gribam strādāt pie konkrētā projekta. */
+
+/* Lai iekoētu jaunajā datorā esošu projektu mums ir jāatver Git Hum repozitorijs, kur ir mums vajadzīgais projekts, jānospiež uz pogas: code, jāizvēlās HTPPS, jānokopē tur dotās adreses ceļš, kas šinī gadījumā ir: 
+https://github.com/Zany1919/Learn_JavaScript.git . Bet mus ir jāatver terminālis un tajā jāizpilda klonēšanas darbība, kas ir šāda: */
+
+//git clone https://github.com/Zany1919/Learn_JavaScript.git
+
+/* Mēs redzam, ka mūsu jaunajā datorā mapītē darbs ir viss mūsu projekts, un mēs varam sākt ar to strādāt. */
+
+/* Strādājam, veicam izmaiņas, papildinam projektu no darba datora. */
